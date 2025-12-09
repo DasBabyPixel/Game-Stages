cd389ef61fdcef4469debe6d3ae16448cfae0f75
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2025-12-09 19:12:05 +0100

    proper archive names

--- build-extensions/src/main/kotlin/loader-version-neoforge.gradle.kts
+++ build-extensions/src/main/kotlin/loader-version-neoforge.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 }
 
 val loaderVersionBase = extensions.getByType<BaseExtension>()
+val versionBaseExtension = extensions.getByType<VersionBaseExtension>()
 val expandProperties = loaderVersionBase.expandProperties
 val extension = extensions.create<LoaderNeoExtension>("loaderVersionNeo", project)
 expandProperties.put("neo_version", provider { extension.neoVersion!! })
@@ -16,6 +17,12 @@ repositories {
 val gameStagesComponent = configurations.named("gameStagesComponent")
 dependencies {
     gameStagesComponent(project(":neoforge"))
+}
+
+tasks.jar {
+    archiveBaseName = "gamestages"
+    archiveVersion = versionBaseExtension.minecraftVersionRange.firstOrNull()
+        ?.let { if (version.toString().isEmpty()) it else "$it-$version" }
 }
 
 neoForge {
